3053747934c0060ce431e370ac300048789eea04
Geoffrey Bilder <gbilder@crossref.org>
2010-12-26 17:27:44 +0100

    Refactored metadataresults, query and record. Added support for RDF transformations

--- spec/cn_proxy_spec.rb
+++ spec/cn_proxy_spec.rb
@@ -10,40 +10,54 @@ describe "Example" do
   def app
     @app ||= Sinatra::Application
   end
-  
+
   it "Returns 500" do
     get '/doidoesnotexist'
     last_response.status.should == 500
     last_response.headers['Content-Type'].should == 'text/html;charset=utf-8'
     last_response.body.should include 'Invalid DOI'
   end
-  
+
   it "/echo_doi/10.1037/0003-066X.59.1.29 using content negotiation for JSON" do
-      get '/echo_doi/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/json'} 
-      last_response.status.should == 200
-      last_response.headers['Content-Type'].should == 'application/json'
-      last_response.body.should == '10.1037/0003-066X.59.1.29' 
+    get '/echo_doi/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/json'} 
+    last_response.status.should == 200
+    last_response.headers['Content-Type'].should == 'application/json'
+    last_response.body.should == '10.1037/0003-066X.59.1.29' 
   end
-  
+
   it "/10.1037/0003-066X.59.1.29 using content negotiation for unixref" do
-      get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/unixref+xml'} 
-      last_response.status.should == 200
-      last_response.headers['Content-Type'].should == 'application/unixref+xml'
-      last_response.body.should include '10.1037/0003-066X.59.1.29' 
+    get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/unixref+xml'} 
+    last_response.status.should == 200
+    last_response.headers['Content-Type'].should == 'application/unixref+xml'
+    last_response.body.should include '10.1037/0003-066X.59.1.29' 
   end
-  
+
   it "/10.1037/0003-066X.59.1.29 using content negotiation for JSON" do
-      get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/json'} 
-      last_response.status.should == 200
-      last_response.headers['Content-Type'].should == 'application/json'
-      last_response.body.should include '10.1037/0003-066X.59.1.29' 
+    get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/json'} 
+    last_response.status.should == 200
+    last_response.headers['Content-Type'].should == 'application/json'
+    last_response.body.should include '10.1037/0003-066X.59.1.29' 
+  end
+
+  it "/10.1037/0003-066X.59.1.29 using content negotiation for ATOM" do
+    get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/atom+xml'} 
+    last_response.status.should == 200
+    last_response.headers['Content-Type'].should == 'application/atom+xml'
+    last_response.body.should include '10.1037/0003-066X.59.1.29' 
+  end
+
+  it "/10.1037/0003-066X.59.1.29 using content negotiation for Turtle" do
+    get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'text/turtle'} 
+    last_response.status.should == 200
+    last_response.headers['Content-Type'].should == 'text/turtle'
+    last_response.body.should include '<info:doi/10.1037/0003-066X.59.1.29>' 
   end
   
-  it "/10.1037/0003-066X.59.1.29 using content negotiation for ATOM" do
-      get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'application/atom+xml'} 
-      last_response.status.should == 200
-      last_response.headers['Content-Type'].should == 'application/atom+xml'
-      last_response.body.should include '10.1037/0003-066X.59.1.29' 
+  it "/10.1037/0003-066X.59.1.29 using content negotiation for N3" do
+    get '/10.1037/0003-066X.59.1.29', {}, {'HTTP_ACCEPT' => 'text/n3'} 
+    last_response.status.should == 200
+    last_response.headers['Content-Type'].should == 'text/n3'
+    last_response.body.should include '<info:doi/10.1037/0003-066X.59.1.29>' 
   end
-   
+
 end
